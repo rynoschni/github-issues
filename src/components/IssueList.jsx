99629eefc25ca51f5526fd35f20782e998fab398
Ryan Schniederjan <ryno@schnietke.com>
2020-10-13 21:39:21 -0500

    Fixed-with hooks

--- src/components/IssueList.jsx
+++ src/components/IssueList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import { loadData } from '../util/loadData'
 import Issue from './Issue';
 
 const IssueList = props => {
